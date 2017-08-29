8553d960124155f57fdb8eb3afdad44bf54ed692
BalaRaman123 <31452769+BalaRaman123@users.noreply.github.com>
2017-08-29 15:40:15 -0400

    Add files via upload
    My first submission for Sales Prediction

---
+++ M2T1_SalesPrediction_Raman.py
@@ -0,0 +1,14 @@
+
+# CTI - 110
+# M2T1 - Sales Prediction
+# Bala
+# 8/28/17
+#
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculated the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $',profit)
